2bf410136df7b0d6452a5ea13b64645e81da289a
Talbekk <mt.campbell@outlook.com>
2022-02-03 21:45:45 +0000

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// The main function is the starting point for all of our apps.
+void main() {
+  runApp(const MaterialApp(
+    home: Center(
+      child: Text('Hello World'),
+    ),
+  ));
+}
